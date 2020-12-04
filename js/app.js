91207d293576c36fe7a9e2385144e568fb4ffafe
Billy Joe Ramos <billyjoersantos@outlook.com>
2020-12-03 22:37:44 -0300

    display changed

--- js/app.js
+++ js/app.js
@@ -29,3 +29,22 @@ Promise.all(pokemonPromises)
     .then(insertPokemonsIntoPage)
 
 fetchPokemon()
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.querySelector('[data-js="pokedex"]');
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
